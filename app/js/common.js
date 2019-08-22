ee940a451fef55f32c564e107c0440e393dcf9d6
Tayirov Bakhtiyar <bakhtiyarkhozha@gmail.com>
2019-08-22 10:04:38 +0500

    fixed menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,14 @@
 $(function() {
 
-	// Custom JS
-	console.log("hello")
+    // menu fixed on scroll
+    $(window).scroll(function() {
+        var scrolled = $(window).scrollTop();
+        if ( scrolled >= 150  ) {
+            $(".header-top-wrap").addClass("fixed");
+
+        } else {
+            $(".header-top-wrap").removeClass("fixed");
+        }
+    });
+
 });
